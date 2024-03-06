ef9ed9a9ade435ca0b489262b63f5a821a9cee7f
Tim Lin <timlin@Tims-MacBook-Pro-2.local>
2024-03-06 19:40:26 +0800

    add firebase files

--- app.js
+++ app.js
@@ -21,3 +21,32 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
+
+
+
+// // Firebase app example
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+//
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB_NZp0ZMtT2h5KHC2RX_JNPDDKwINb2C8",
+//   authDomain: "wedding-223be.firebaseapp.com",
+//   projectId: "wedding-223be",
+//   storageBucket: "wedding-223be.appspot.com",
+//   messagingSenderId: "950666858866",
+//   appId: "1:950666858866:web:37d258e8b1fca3fc518deb",
+//   measurementId: "G-EJ2LCXKHKM"
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+// Run $firebase deploy
